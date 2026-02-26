1dfb8b489ddb47ae6e27f7fa9e178c5181821539
Froggles <elmsterm@gmail.com>
2026-02-26 11:35:26 -0500

    add post tasks route

--- api/tasks.js
+++ api/tasks.js
@@ -2,6 +2,14 @@ import { Router } from "express";
 const router = Router();
 export default router;
 
+import requireBody from "#middleware/requireBody";
 import requireUser from "#middleware/requireUser";
+import { createTask } from "#db/queries/tasks";
 
 router.use(requireUser);
+
+router.post("/", requireBody(["title", "done"]), async (req, res) => {
+  const { title, done } = req.body;
+  const task = await createTask(title, req.user.id, done);
+  res.status(201).send(task);
+});
