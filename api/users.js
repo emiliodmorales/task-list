ee49932864d21a33719df22de97bb7cf2a170cad
Froggles <elmsterm@gmail.com>
2026-02-26 11:28:20 -0500

    create the login route

--- api/users.js
+++ api/users.js
@@ -4,7 +4,7 @@ export default router;
 
 import requireBody from "#middleware/requireBody";
 import { createToken } from "#utils/jwt";
-import { createUser } from "#db/queries/users";
+import { createUser, getUserByLogin } from "#db/queries/users";
 
 router.post(
   "/register",
@@ -14,5 +14,17 @@ router.post(
     const user = await createUser(username, password);
     const token = await createToken({ id: user.id });
     res.status(201).send(token);
+  },
+);
+
+router.post(
+  "/login",
+  requireBody(["username", "password"]),
+  async (req, res) => {
+    const { username, password } = req.body;
+    const user = await getUserByLogin(username, password);
+    if (!user) return res.sendStatus(401);
+    const token = await createToken({ id: user.id });
+    res.send(token);
   },
 );
