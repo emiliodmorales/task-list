d9148f6a2c32669112f279da51af2edd04ffc5f4
Froggles <elmsterm@gmail.com>
2026-02-26 11:23:13 -0500

    create the register route

--- api/users.js
+++ api/users.js
@@ -1,3 +1,18 @@
 import { Router } from "express";
 const router = Router();
 export default router;
+
+import requireBody from "#middleware/requireBody";
+import { createToken } from "#utils/jwt";
+import { createUser } from "#db/queries/users";
+
+router.post(
+  "/register",
+  requireBody(["username", "password"]),
+  async (req, res) => {
+    const { username, password } = req.body;
+    const user = await createUser(username, password);
+    const token = await createToken({ id: user.id });
+    res.status(201).send(token);
+  },
+);
