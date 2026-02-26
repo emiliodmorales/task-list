9b34ea80dd32dcb5ae9eb252ba92e170a9c11e0a
Froggles <elmsterm@gmail.com>
2026-02-26 11:46:59 -0500

    create router param to attach the task

--- api/tasks.js
+++ api/tasks.js
@@ -4,7 +4,7 @@ export default router;
 
 import requireBody from "#middleware/requireBody";
 import requireUser from "#middleware/requireUser";
-import { createTask, getTasksByUser } from "#db/queries/tasks";
+import { createTask, getTasksByUser, getTaskById } from "#db/queries/tasks";
 
 router.use(requireUser);
 
@@ -17,4 +17,20 @@ router.post("/", requireBody(["title", "done"]), async (req, res) => {
 router.get("/", async (req, res) => {
   const tasks = await getTasksByUser(req.user.id);
   res.send(tasks);
+});
+
+router.param("id", async (req, res, next, id) => {
+  if (Number.isNaN(id))
+    return res.status(400).send("Task id must be a positive integer.");
+
+  const task = await getTaskById(id);
+  if (!task) return res.status(404).send("Task not found.");
+  if (task.user_id !== req.user.id) return res.sendStatus(403);
+
+  req.task = task;
+  next();
+});
+
+router.put("/:id", requireBody(["title", "done"]), async (req, res) => {
+  const { title, done } = req.body;
 });
