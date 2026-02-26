7af763e55ccfd673fcafd5d9711471eec2924bda
Froggles <elmsterm@gmail.com>
2026-02-26 11:13:22 -0500

    create the create task query

---
+++ db/queries/tasks.js
@@ -0,0 +1,20 @@
+import db from "#db/client";
+
+/**
+ * Create a new record in the tasks table
+ * @param {string} title - The title of the next task
+ * @param {number} userId - The id of the user making the task
+ * @param {boolean} done - Whether the task has been done
+ * @returns the newly created task
+ */
+export async function createTask(title, userId, done) {
+  const sql = `
+    INSERT INTO tasks (title, user_id, done)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `;
+  const {
+    rows: [task],
+  } = await db.query(sql, [title, userId, done]);
+  return task;
+}
